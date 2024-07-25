a89b23d5ed77360bbcf23587b4124d685fb6ff41
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-25 03:08:37 +0000

    final debug!

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -34,7 +34,7 @@ class Photo < ApplicationRecord
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
 
-  has_many(:fans, through: :likes, source: :user)
+  has_many(:fans, through: :likes, source: :fan)
 
   ###########
 
